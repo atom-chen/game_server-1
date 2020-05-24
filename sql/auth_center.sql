38314c51982ba605834b845d593d4ac4064eb379
hsc <827773271@qq.com>
2020-05-25 01:07:27 +0800

    add sql file

--- sql/auth_center.sql
+++ sql/auth_center.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50168
-Source Host           : localhost:3306
+Source Server         : hccfun
+Source Server Version : 50505
+Source Host           : 121.41.0.245:3306
 Source Database       : auth_center
 
 Target Server Type    : MYSQL
-Target Server Version : 50168
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2019-11-18 00:19:15
+Date: 2020-05-25 01:06:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,36 +23,55 @@ CREATE TABLE `uinfo` (
   `uid` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '玩家唯一的UID号,仅供服务端识别使用',
   `numberid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '玩家numberid,识别第几号用户',
   `areaid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '地区areaid,识别地区',
-  `unick` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的昵称',
+  `unick` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的昵称',
   `usex` int(8) NOT NULL DEFAULT '0' COMMENT '0:男, 1:女的',
   `uface` int(8) NOT NULL DEFAULT '0' COMMENT '系统默认图像，自定义图像后面再加上',
   `uname` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的账号名称',
   `upwd` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家密码的MD5值',
   `uphone` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的电话',
   `uemail` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的email',
-  `address` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的地址',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '玩家的地址',
   `uvip` int(8) NOT NULL DEFAULT '0' COMMENT '玩家VIP的等级，这个是最普通的',
   `vip_endtime` int(32) NOT NULL DEFAULT '0' COMMENT '玩家VIP到期的时间撮',
   `is_guest` int(8) NOT NULL DEFAULT '0' COMMENT '标志改账号是否为游客账号',
   `guest_key` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '游客账号的唯一的key',
   `status` int(8) NOT NULL DEFAULT '0' COMMENT '0表示正常，其他根据需求定',
+  `avatarurl` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '微信头像地址',
+  `unionid` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '""' COMMENT '微信唯一unionid',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='存放我们的玩家信息';
+) ENGINE=InnoDB AUTO_INCREMENT=1932 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='存放我们的玩家信息';
 
 -- ----------------------------
 -- Records of uinfo
 -- ----------------------------
-INSERT INTO `uinfo` VALUES ('1', '1000001', '0', '肖炎', '0', '1', 'xiaoyan', '111111', '13066666666', '827773271@qq.com', '杭州下城凤栖花园', '0', '0', '0', 'cYC8E6BQBHjTc5ka55H5hj3wKbJHN5hf', '0');
-INSERT INTO `uinfo` VALUES ('2', '1000002', '0', 'huangshucheng', '1', '2', 'hccfun', '111111', '13067702010', '3333333@qq.com', '杭州下城凤栖花园', '0', '0', '0', '1', '0');
-INSERT INTO `uinfo` VALUES ('3', '1000003', '0', '测试1111', '0', '3', 'test1111', '111111', '13033333333', '\"\"', '\"\"', '0', '0', '0', '2', '0');
-INSERT INTO `uinfo` VALUES ('95', '1000004', '0', 'test2222', '1', '3', 'test2222', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', 'testkey123456', '0');
-INSERT INTO `uinfo` VALUES ('96', '1000005', '0', 'test3333', '1', '1', '13065525252', '123456789123456789123456', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0');
-INSERT INTO `uinfo` VALUES ('97', '1000006', '0', 'test3333', '1', '1', '13065525252', '123456789123456789123456', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0');
-INSERT INTO `uinfo` VALUES ('98', '1000007', '0', 'test3333', '1', '1', '13065525252', '123456789123456789123456', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0');
-INSERT INTO `uinfo` VALUES ('111', '1000111', '0', 'gst41939', '1', '8', 'test1139', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0');
-INSERT INTO `uinfo` VALUES ('112', '1000112', '0', 'gst95371', '0', '9', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'XrrxbHapZmyj88EeTmzyhEcGja23he54', '0');
-INSERT INTO `uinfo` VALUES ('113', '1000113', '0', 'gst02644', '1', '1', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'P3zEeQYYBrx3aJyp35XcrYeQpKRBkZs7', '0');
-INSERT INTO `uinfo` VALUES ('114', '1000114', '0', 'gst07852', '1', '2', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'zNDBjrw2tCMfttSnwTW42PWjhrKiHxTS', '0');
-INSERT INTO `uinfo` VALUES ('115', '1000115', '0', 'gst67440', '1', '3', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', '3ETABzshjbZJhteKBQppR6Gc46ezpJ6b', '0');
-INSERT INTO `uinfo` VALUES ('116', '1000116', '0', 'gst01829', '1', '4', 'test1138', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0');
-INSERT INTO `uinfo` VALUES ('117', '1000117', '0', 'gst42083', '0', '6', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'DpCJcH2d6BmXCmQHnBZJ6KFcaC8Wc3aa', '0');
+INSERT INTO `uinfo` VALUES ('1899', '1000001', '0', 'user1000001', '0', '3', 'test1111', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1901', '1001901', '0', 'user1001901', '0', '9', 'test2222', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1902', '1001902', '0', 'user1001902', '0', '2', 'test3333', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1903', '1001903', '0', 'user1001903', '1', '9', 'test4444', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1904', '1001904', '0', 'user1001904', '0', '3', 'test5555', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1905', '1001905', '0', 'user1001905', '1', '4', 'test6666', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1906', '1001906', '0', 'user1001906', '1', '8', 'test7777', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1907', '1001907', '0', 'user1001907', '1', '5', 'test8888', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1908', '1001908', '0', 'user1001908', '1', '8', 'test9999', '111111', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1910', '1001909', '0', 'C小C????', '1', '0', '\"\"', '\"\"', '\"\"', '\"\"', 'China-Zhejiang-Hangzhou', '0', '0', '0', '0', '0', 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLQoDgahGVTFb6H74TZz9z5OI9RoXWmXJ6WXbWsfvWKCAD5KCdaYfdJZCf8aR0N4oP5bKXImelbkw/132', 'oaCkmwOd91uU-3oX78pJ59PFndGs');
+INSERT INTO `uinfo` VALUES ('1911', '1001911', '0', 'guest1001911', '0', '4', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'TnhSy7aP7TkK7MN2rppYTs2aKM6jicF7', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1912', '1001912', '0', 'guest1001912', '0', '9', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'nM5irPKdfYX6dJP8RPJKR5G6f7ZGx2pe', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1913', '1001913', '0', 'guest1001913', '0', '8', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'C4wkp84z4TbMJbmfyaYmYcCW2em43Biz', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1914', '1001914', '0', 'guest1001914', '0', '8', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'pfhndH8A6ebMM225p5kdMn5sDeK2xNsF', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1915', '1001915', '0', '大虾米', '0', '0', '\"\"', '\"\"', '\"\"', '\"\"', 'unknown-unknown-unknown', '0', '0', '0', '0', '0', 'https://wx.qlogo.cn/mmopen/vi_32/OKWDZRD96YmF1xyTpMvrXDUs1fPJBnFibCGrWD1EuKO3jaIJVUamv8BoVLpDkSAzz1qTfwicYWY6CZrPicuqq4UIQ/132', 'oaCkmwJH3e_jDNRM-kc4K-1OhzY8');
+INSERT INTO `uinfo` VALUES ('1916', '1001916', '0', '小星星', '1', '0', '\"\"', '\"\"', '\"\"', '\"\"', 'Poland-unknown-unknown', '0', '0', '0', '0', '0', 'https://wx.qlogo.cn/mmopen/vi_32/rnzvIFJbbVnlXic31C7fUibWeyN1ys8s7tPRsdSTF30eGyhzBHUmmg8uR6swL0aLnK4LcAsJpzCh1gC5dDUW3GaA/132', 'oaCkmwH6yIkReyRoIec5nIg7_Ebw');
+INSERT INTO `uinfo` VALUES ('1917', '1001917', '0', 'guest1001917', '0', '2', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'HdGt5d73rWwQ2DwYMwEXSPGEtpyFPPH3', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1918', '1001918', '0', 'guest1001918', '0', '4', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'pznEXfB3xTJXR5M7AXSw74sM3GWNCQkd', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1919', '1001919', '0', 'guest1001919', '1', '9', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'i5aANJrF5tDQHJYS8knZJJBEx3Amw4Bd', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1920', '1001920', '0', 'guest1001920', '1', '3', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'xxPipwADGPHJnsDxsJDYQysDD28WxSWZ', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1921', '1001921', '0', 'guest1001921', '0', '7', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'TREGmnW3tBsDhbBHy5YJZ5RdZZZC7QSJ', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1922', '1001922', '0', 'guest1001922', '1', '2', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'XQBAdph5wnXEJX4z838Nef6He2kaMGAP', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1923', '1001923', '0', 'guest1001923', '0', '1', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'TD7z3WdpTCH2i2Cc8sYhQaWa6fJJrHHJ', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1924', '1001924', '0', 'guest1001924', '1', '5', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'pBGsjKnyG6yWrYRpe244mJi8fizAMHzC', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1925', '1001925', '0', 'guest1001925', '1', '9', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'skNPcfbbJpFfxtsEsa7abhWHC7jGEED7', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1926', '1001926', '0', 'user1001926', '1', '5', 'wanxiao123', 'wanxiao', '\"\"', '\"\"', '\"\"', '0', '0', '0', '0', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1927', '1001927', '0', 'guest1001927', '0', '9', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'szd3mTZK4N3MYztaEE3XY2f2xcF6f6AB', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1928', '1001928', '0', 'guest1001928', '1', '1', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'ADG72Kn4rGcMwFRsGpEeS3zKPa3PpaYk', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1929', '1001929', '0', 'guest1001929', '0', '1', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'j4HHRRGHpSSwKnYDkDMns7rBZPezKXhX', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1930', '1001930', '0', 'guest1001930', '1', '3', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'rbZ5PykwYrTHT358tZaSNW4bQTi8mQGj', '0', '\"\"', '\"\"');
+INSERT INTO `uinfo` VALUES ('1931', '1001931', '0', 'guest1001931', '1', '8', '\"\"', '\"\"', '\"\"', '\"\"', '\"\"', '0', '0', '1', 'rSPHztFfZ3mSGs3j7ZtjhxDG7wHADrAb', '0', '\"\"', '\"\"');
--- sql/moba_game.sql
+++ sql/moba_game.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50168
-Source Host           : localhost:3306
+Source Server         : hccfun
+Source Server Version : 50505
+Source Host           : 121.41.0.245:3306
 Source Database       : moba_game
 
 Target Server Type    : MYSQL
-Target Server Version : 50168
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2019-11-17 00:13:46
+Date: 2020-05-25 01:06:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,6 +30,10 @@ CREATE TABLE `login_bonues` (
 ) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of login_bonues
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for sys_msg
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_msg`;
@@ -38,6 +42,10 @@ CREATE TABLE `sys_msg` (
   `msg` varchar(255) DEFAULT '' COMMENT '公告文字',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_msg
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for ugame
@@ -56,5 +64,50 @@ CREATE TABLE `ugame` (
   `udata3` int(11) NOT NULL DEFAULT '0' COMMENT '用户在游戏中的道具3',
   `uexp` int(11) NOT NULL DEFAULT '0' COMMENT '用户的经验值',
   `ustatus` int(11) NOT NULL DEFAULT '0' COMMENT '0正常，其他为不正常',
+  `uball_info` varchar(1024) NOT NULL DEFAULT '' COMMENT '玩家小球信息',
+  `user_config` varchar(1024) NOT NULL DEFAULT '' COMMENT '玩家配置',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COMMENT='moba游戏数据表, 存放用户的游戏数据';
+) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8 COMMENT='moba游戏数据表, 存放用户的游戏数据';
+
+-- ----------------------------
+-- Records of ugame
+-- ----------------------------
+INSERT INTO `ugame` VALUES ('53', '143', '1500', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('54', '148', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=0&lv_2=2&lv_3=1&lv_4=2&lv_5=2&lv_6=0&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('55', '139', '1900', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('56', '145', '2390', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=1&lv_4=1&lv_5=2&lv_6=1&lv_7=3&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('57', '147', '1500', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('58', '146', '7910', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=15&lv_2=2&lv_3=2&lv_4=2&lv_5=2&lv_6=1&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('59', '149', '9540', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=0&lv_2=1&lv_3=4&lv_4=5&lv_5=1&lv_6=1&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('60', '138', '9910', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=6&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('61', '173', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('62', '156', '8930', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=0&lv_3=1&lv_4=4&lv_5=1&lv_6=0&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('63', '1910', '8600', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=2&lv_3=0&lv_4=0&lv_5=2&lv_6=1&lv_7=0&lv_8=0&lv_9=0&lv_10=1', 'user_ball_level=5');
+INSERT INTO `ugame` VALUES ('64', '1907', '10300', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('65', '1911', '10300', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=0&lv_2=1&lv_3=4&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('66', '1912', '9700', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('67', '1913', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('68', '1899', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=0&lv_3=4&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=0', '');
+INSERT INTO `ugame` VALUES ('69', '1914', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('70', '1915', '9800', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=0&lv_3=4&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('71', '1916', '10600', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=0&lv_2=1&lv_3=1&lv_4=1&lv_5=1&lv_6=0&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('72', '1902', '10130', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=4&lv_3=8&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('73', '1917', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('74', '1918', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('75', '1919', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('76', '1908', '9900', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('78', '1901', '9720', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=9&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', 'user_ball_level=4');
+INSERT INTO `ugame` VALUES ('79', '1920', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('80', '1921', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('81', '1922', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('82', '1923', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('83', '1924', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('84', '1925', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=0&lv_5=4&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('85', '1926', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('86', '1927', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('87', '1928', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('88', '1904', '10200', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=0&lv_2=1&lv_3=1&lv_4=1&lv_5=1&lv_6=0&lv_7=0&lv_8=0&lv_9=0&lv_10=1', '');
+INSERT INTO `ugame` VALUES ('89', '1929', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('90', '1906', '9900', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('91', '1930', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
+INSERT INTO `ugame` VALUES ('92', '1931', '10000', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'lv_1=3&lv_2=3&lv_3=3&lv_4=3&lv_5=3&lv_6=2&lv_7=2&lv_8=2&lv_9=2', '');
