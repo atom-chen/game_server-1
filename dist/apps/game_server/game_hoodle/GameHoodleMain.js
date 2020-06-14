9d92ef7e3a9a9eaacf56e114b18eeff8a3f07af4
hsc <827773271@qq.com>
2020-06-14 23:08:16 +0800

    内存使用，打印一下

--- dist/apps/game_server/game_hoodle/GameHoodleMain.js
+++ dist/apps/game_server/game_hoodle/GameHoodleMain.js
@@ -15,6 +15,7 @@ var GameHoodleService_1 = __importDefault(require("./GameHoodleService"));
 var MySqlAuth_1 = __importDefault(require("../../../database/MySqlAuth"));
 var MatchManager_1 = __importDefault(require("./MatchManager"));
 var GameAppConfig_1 = __importDefault(require("../../config/GameAppConfig"));
+var Log_1 = __importDefault(require("../../../utils/Log"));
 var game_server = GameAppConfig_1["default"].game_server;
 NetBus_1["default"].start_tcp_server(game_server.host, game_server.port, false);
 ServiceManager_1["default"].register_service(Stype_1.Stype.GameHoodle, GameHoodleService_1["default"]);
@@ -26,4 +27,17 @@ var db_auth = GameAppConfig_1["default"].auth_database;
 MySqlAuth_1["default"].connect(db_auth.host, db_auth.port, db_auth.db_name, db_auth.uname, db_auth.upwd);
 //匹配场
 MatchManager_1["default"].getInstance().start_match();
+//内存使用打印
+function print_memery() {
+    var memUsage = process.memoryUsage();
+    var cpuUsage = process.cpuUsage();
+    var mem_format = function (bytes) {
+        return (bytes / 1024 / 1024).toFixed(2) + 'MB';
+    };
+    Log_1["default"].info('memeryUsage: heapTotal(' + mem_format(memUsage.heapTotal) + ') ,heapUsed(' + mem_format(memUsage.heapUsed) + ') ,rss(' + mem_format(memUsage.rss) + ")");
+    Log_1["default"].info("cpuUsage: system(" + mem_format(cpuUsage.system) + ") ,user(" + mem_format(cpuUsage.user) + ")");
+}
+// setInterval(function() {
+// 	print_memery();
+// },1000);
 //# sourceMappingURL=GameHoodleMain.js.map
--- src/apps/game_server/game_hoodle/GameHoodleMain.ts
+++ src/apps/game_server/game_hoodle/GameHoodleMain.ts
@@ -11,6 +11,7 @@ import GameHoodleService from './GameHoodleService';
 import MySqlAuth from '../../../database/MySqlAuth';
 import MatchManager from './MatchManager';
 import GameAppConfig from '../../config/GameAppConfig';
+import Log from '../../../utils/Log';
 
 let game_server = GameAppConfig.game_server;
 NetBus.start_tcp_server(game_server.host, game_server.port, false);
@@ -26,4 +27,19 @@ var db_auth = GameAppConfig.auth_database;
 MySqlAuth.connect(db_auth.host, db_auth.port, db_auth.db_name, db_auth.uname, db_auth.upwd)
 
 //匹配场
-MatchManager.getInstance().start_match()
+MatchManager.getInstance().start_match();
+
+//内存使用打印
+function print_memery() {
+	var memUsage = process.memoryUsage();
+	let cpuUsage = process.cpuUsage();
+	var mem_format = function (bytes:number) {
+		return (bytes / 1024 / 1024).toFixed(2) + 'MB';
+	};
+	Log.info('memeryUsage: heapTotal(' + mem_format(memUsage.heapTotal) + ') ,heapUsed(' + mem_format(memUsage.heapUsed) + ') ,rss(' + mem_format(memUsage.rss) + ")");
+	Log.info("cpuUsage: system("+ mem_format(cpuUsage.system)+ ") ,user("+  mem_format(cpuUsage.user) + ")");
+}
+
+// setInterval(function() {
+// 	print_memery();
+// },1000);
